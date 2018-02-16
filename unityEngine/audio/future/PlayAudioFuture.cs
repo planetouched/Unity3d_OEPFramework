4a6074b4b1f76cac226be2a771ef2dbe47403fc5
DESKTOP-CDCKJEM\solar <sourcecode15619@gmail.com>
2018-02-16 11:19:43 +0300

    audioplayer

---
+++ unityEngine/audio/future/PlayAudioFuture.cs
@@ -0,0 +1,84 @@
+﻿using OEPFramework.common.future.utils;
+using OEPFramework.unityEngine.behaviour;
+using OEPFramework.unityEngine.loop;
+using UnityEngine;
+
+namespace OEPFramework.unityEngine.audio.future
+{
+    public class PlayAudioFuture : FutureBehaviour
+    {
+        public AudioSource audioSource { get; private set; }
+        private readonly AudioClip clip;
+        private readonly FutureScenario scenario;
+        
+        public PlayAudioFuture(AudioSource audioSource, AudioClip clip)
+        {
+            this.audioSource = audioSource;
+            this.clip = clip;
+        }
+
+        public PlayAudioFuture(AudioSource audioSource, AudioClip[] clips)
+        {
+            this.audioSource = audioSource;
+            scenario = new FutureScenario();
+            foreach (var c in clips)
+            {
+                scenario.AddFuture(new PlayAudioFuture(audioSource, c));
+                scenario.Next();
+            }
+        }
+
+        protected override void OnRun()
+        {
+            if (scenario != null)
+            {
+                scenario.onComplete += cancel =>
+                {
+                    if (!cancel)
+                        Complete();
+                };
+                scenario.Run();
+            }
+            else
+            {
+                if (clip != null)
+                    audioSource.clip = clip;
+
+                audioSource.Play();
+
+                LoopOn(Loops.UPDATE, Update);
+                Play();
+            }
+        }
+
+        private void Update()
+        {
+            if (audioSource == null)
+            {
+                Cancel();
+                return;
+            }
+
+            if (!audioSource.loop && !audioSource.isPlaying)
+                Complete();
+        }
+
+        protected override void OnComplete()
+        {
+            if (dropped) return;
+
+            if (scenario == null)
+            {
+                audioSource.Stop();
+
+                if (clip != null)
+                    audioSource.clip = null;
+            }
+            else
+            {
+                if (isCancelled)
+                    scenario.Cancel();
+            }
+        }
+    }
+}
--- unityEngine/loop/Loops.cs
+++ unityEngine/loop/Loops.cs
@@ -2,10 +2,19 @@
 {
     public static partial class Loops
     {
-        public static int TIMER = EngineLoopManager.AddNewEngineLoop();
-        public static int FIXED_UPDATE = EngineLoopManager.AddNewEngineLoop();
-        public static int UPDATE = EngineLoopManager.AddNewEngineLoop();
-        public static int LATE_UPDATE = EngineLoopManager.AddNewEngineLoop();
-        public static int LEGACY_GUI = EngineLoopManager.AddNewEngineLoop();
+        public static int TIMER;
+        public static int FIXED_UPDATE;
+        public static int UPDATE;
+        public static int LATE_UPDATE;
+        public static int LEGACY_GUI;
+
+        public static void Init()
+        {
+            TIMER = EngineLoopManager.AddNewEngineLoop();
+            FIXED_UPDATE = EngineLoopManager.AddNewEngineLoop();
+            UPDATE = EngineLoopManager.AddNewEngineLoop();
+            LATE_UPDATE = EngineLoopManager.AddNewEngineLoop();
+            LEGACY_GUI = EngineLoopManager.AddNewEngineLoop();
+        }
     }
 }
